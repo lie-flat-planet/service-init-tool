c2b83e11456481f575aef3a7da26ca1dcf342671
xiaoxlm <39261724+xiaoxlm@users.noreply.github.com>
2024-10-14 15:44:28 +0800

    feat: add mysql delet time

--- component/mysql/time.go
+++ component/mysql/time.go
@@ -2,27 +2,40 @@ package mysql
 
 import (
 	"database/sql/driver"
+	"encoding/json"
 	"fmt"
+	"gorm.io/gorm"
 	"time"
 )
 
-type Time struct {
+type NormalTime struct {
 	time.Time
 }
 
-func (t Time) MarshalJSON() ([]byte, error) {
+func (t NormalTime) MarshalJSON() ([]byte, error) {
 	formatted := fmt.Sprintf("\"%s\"", t.Format("2006-01-02 15:04:05"))
 	return []byte(formatted), nil
 }
 
-func (t *Time) Scan(value interface{}) error {
+func (t *NormalTime) Scan(value interface{}) error {
 	if v, ok := value.(time.Time); ok {
-		*t = Time{Time: v}
+		*t = NormalTime{Time: v}
 		return nil
 	}
 	return fmt.Errorf("failed to scan time value: %v", value)
 }
 
-func (t Time) Value() (driver.Value, error) {
+func (t NormalTime) Value() (driver.Value, error) {
 	return t.Time, nil
+}
+
+type DeletedTime struct {
+	gorm.DeletedAt
+}
+
+func (delete DeletedTime) MarshalJSON() ([]byte, error) {
+	if delete.Valid {
+		return json.Marshal(delete.Time)
+	}
+	return json.Marshal("")
 }
