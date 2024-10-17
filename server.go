d735c7dd1b486ccbce6e94bd349f6a7f7103b019
xiaoxlm <39261724+xiaoxlm@users.noreply.github.com>
2024-10-17 09:23:38 +0800

    feat: add gin http server

--- server.go
+++ server.go
@@ -1,9 +1,18 @@
 package service_init_tool
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/lie-flat-planet/service-init-tool/enum"
 	"github.com/lie-flat-planet/service-init-tool/log"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type Server struct {
@@ -14,6 +23,8 @@ type Server struct {
 	LogLevel string `env:""`
 	HttpPort uint   `env:""`
 	RunMode  string `env:""`
+
+	httpServer *http.Server
 }
 
 func (s *Server) GetHttpPort() uint {
@@ -60,4 +71,38 @@ func (s *Server) check() error {
 	}
 
 	return nil
+}
+
+func (s *Server) GinServe(engine *gin.Engine) {
+	port := fmt.Sprintf(":%d", s.HttpPort)
+	s.httpServer = &http.Server{
+		Addr:    port,
+		Handler: engine,
+	}
+
+	//start server
+	go func() {
+		logrus.Println("Starting server on " + port + "...")
+		if err := s.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logrus.Fatalf("server startup error. err:%v", err)
+		}
+	}()
+
+	// gracefulShutdown
+	s.gracefulShutdown()
+}
+
+func (s *Server) gracefulShutdown() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+	logrus.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		logrus.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	logrus.Println("Server exiting")
 }
